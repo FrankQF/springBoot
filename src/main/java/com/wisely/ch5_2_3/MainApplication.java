bdae181c93b3262dc9cc0d2966edb80ffd25930b
齐放 <qi.fang@neusoft.com>
2017-10-21 21:34:01 +0800

    first commit

---
+++ src/main/java/com/wisely/ch5_2_3/MainApplication.java
@@ -0,0 +1,16 @@
+package com.wisely.ch5_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
